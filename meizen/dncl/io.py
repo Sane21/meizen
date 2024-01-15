47c3d3b8eb61c9817be8f4297d00f1b45d4a8b6d
Sane21 <72343236+Sane21@users.noreply.github.com>
2024-01-15 19:39:22 +0900

    ファイルの書き込み機能の作成

--- meizen/dncl/io.py
+++ meizen/dncl/io.py
@@ -1,4 +1,15 @@
+# 読み込み
 def load(path: str) -> list:
     file = open(path, 'r')
     data = file.readlines()
+    file.close()
     return data
+
+
+# 書き込み
+def write(path: str, code_list: list):
+    file = open(path, 'w')
+    for code in code_list:
+        file.write(code)
+    file.close()
+    return
