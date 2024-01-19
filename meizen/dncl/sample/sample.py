877c3d1a86d1d6d539f0411c1318f17304367554
Sane21 <72343236+Sane21@users.noreply.github.com>
2024-01-19 22:01:18 +0900

    関数の挙動を検証

--- meizen/dncl/sample/sample.py
+++ meizen/dncl/sample/sample.py
@@ -1,9 +1,17 @@
-def Maisu():
+Kouka = [1,5,10,50,100]
+kingaku = 46
+maisu = 0
+nokori = kingaku
+for i  in range( 4 ,  0 , -1):
+	maisu = maisu + nokori // Kouka[i]
+	nokori = nokori % Kouka[i]
+print(maisu)
+
+def Maisu(kingaku):
 	Kouka = [1,5,10,50,100]
-	kingaku = 46
 	maisu = 0
 	nokori = kingaku
 	for i  in range( 4 ,  0 , -1):
 		maisu = maisu + nokori // Kouka[i]
 		nokori = nokori % Kouka[i]
-	print(maisu)
+	return maisu
--- meizen/dncl/sample/sample2.py
+++ meizen/dncl/sample/sample2.py
@@ -1,10 +1,10 @@
-from .sample import Maisu
+from sample import Maisu
 
 kakaku = 46
 min_maisu = 100
-for tsuri  in range( 1 ,  99 ,  1 ):
-    shiharai = kakaku + tsuri
-    maisu = kakaku + kakaku
-    if maisu < min_maisu :
-        maisu = maisu
-print(min_maisu)
+for tsuri  in range( 0 ,  99 ,  1 ):
+	shiharai = kakaku + tsuri
+	maisu = Maisu(shiharai) + Maisu(tsuri)
+	if maisu < min_maisu :
+		maisu = maisu
+print(maisu)
