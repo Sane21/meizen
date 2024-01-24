d167b2b9636de61c91cf6200259b4f06a3560570
Sane21 <72343236+Sane21@users.noreply.github.com>
2024-01-24 18:28:12 +0900

    明示的に実行結果を表示するように変更

--- meizen/util/run.py
+++ meizen/util/run.py
@@ -3,12 +3,14 @@ import subprocess
 
 def run(path: str):
     command = ["python", path]
-    subprocess.run(command)
+    output = subprocess.run(command, shell=False, capture_output=True, text=True).stdout
     print("コマンドを実行: " + str(command))
+    print(output)
 
 
 def run_args(path: str, args: list[str]):
     command = ["python", path]
     command += args
-    subprocess.run(command)
+    output = subprocess.run(command, shell=False, capture_output=True, text=True).stdout
     print("コマンドを実行: " + str(command))
+    print(output)
