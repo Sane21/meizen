7327a2847e177ad2027167c16c4a22c419029752
Keita Sanefuji <72343236+Sane21@users.noreply.github.com>
2024-03-01 17:20:18 +0900

    make_runでエラーも表示

--- meizen/util/run.py
+++ meizen/util/run.py
@@ -3,14 +3,20 @@ import subprocess
 
 def run(path: str):
     command = ["python", path]
-    output = subprocess.run(command, shell=False, capture_output=True, text=True).stdout
+    result = subprocess.run(command, shell=False, capture_output=True, text=True)
+    output = result.stdout
+    err = result.stderr
     print("コマンドを実行: " + str(command))
     print(output)
+    print(err)
 
 
 def run_args(path: str, args: list[str]):
     command = ["python", path]
     command += args
-    output = subprocess.run(command, shell=False, capture_output=True, text=True).stdout
+    result = subprocess.run(command, shell=False, capture_output=True, text=True)
+    output = result.stdout
+    err = result.stderr
     print("コマンドを実行: " + str(command))
     print(output)
+    print(err)
