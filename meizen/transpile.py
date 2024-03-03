9242bbd46b7a67838c5c8a3f68bfde6558ae6ae6
Keita Sanefuji <readbook2002h14@gmail.com>
2024-03-03 16:28:59 +0900

    答えの確認を行うexam_str, exam_reの実装

--- meizen/transpile.py
+++ meizen/transpile.py
@@ -1,5 +1,6 @@
 from .dncl.lexer import compile_code
 from .util.run import run as util_run
+from .util.regEx import check_str, check_re
 
 
 def make(path: str, filename: str):
@@ -24,17 +25,18 @@ def build(path: str, filename: str):
     compile_code(path=path, filename=filename)
 
 
-def run(path: str, filename: str):
+def run(path: str, filename: str) -> (str, str):
     """
     DNCLからPythonへの翻訳-実行関数
     pathフォルダ内に存在するfilename.dnclを読み取り、同フォルダ内にfilename.pyを作成し、filename.pyを実行する
     作成部はbuildに委譲
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename: 読み取るファイル名 (拡張子は除く)
-    :return:
+    :return: 実行結果の標準出力, エラー出力
     """
     build(path=path, filename=filename)
-    util_run(path=path + filename + ".py")
+    result = util_run(path=path + filename + ".py")
+    return result
 
 
 def make_run(path: str, filename: str):
@@ -45,3 +47,43 @@ def make_run(path: str, filename: str):
     :param filename: 読み取るファイル名 (拡張子は除く)
     """
     run(path, filename)
+
+
+def exam_re(path: str, filename: str, answer: str):
+    """
+    変換と実行、実行結果の正規表現による確認まで行う関数
+    python標準のreパッケージのfullmatchに基づく
+    :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
+    :param filename:読み取るファイル名 (拡張子は除く)
+    :param answer: 確認する正規表現
+    :return:
+    """
+    out, _ = run(path=path, filename=filename)
+    result: bool = check_re(sentence=out, pattern=answer)
+    print(":----------出力を確認します----------:")
+    if result:
+        print("出力は正常です.")
+    else:
+        print("出力に不備があります.想定される出力は次の通りです.")
+        print(answer)
+    print(":-----------確認は以上です-----------:")
+
+
+def exam_str(path: str, filename: str, answer: str):
+    """
+    変換と実行、実行結果の文字列による確認まで行う関数
+    完全一致のみを正答とする
+    :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
+    :param filename:読み取るファイル名 (拡張子は除く)
+    :param answer: 確認する文字列
+    :return:
+    """
+    out, _ = run(path=path, filename=filename)
+    result: bool = check_str(sentence=out, string=answer)
+    print(":----------出力を確認します----------:")
+    if result:
+        print("出力は正常です.")
+    else:
+        print("出力に不備があります.想定される出力は次の通りです.")
+        print(answer)
+    print(":-----------確認は以上です-----------:")
