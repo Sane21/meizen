20b70eef49665b9e03898cea8e348bcc993722b9
Keita Sanefuji <72343236+Sane21@users.noreply.github.com>
2024-03-10 23:16:53 +0900

    記録を保存する機能の実装 (#16)
    * 表示の処理を抽出
    
    * Logger型の定義
    
    * 書き込みの種類を選択可能に変更
    
    * Loggerの使用を記述

--- meizen/__init__.py
+++ meizen/__init__.py
@@ -1,4 +1,7 @@
 from .display import print_while, print_for, print_if
 from .transpile import make, make_run, build, run, exam_re, exam_str
+from .util.log import Logger
 
-__all__ = ['make', 'make_run', 'print_while', 'print_for', 'print_if', 'build', 'run', 'exam_re', 'exam_str']
+__all__ = ['make', 'make_run',
+           'print_while', 'print_for', 'print_if',
+           'build', 'run', 'exam_re', 'exam_str', 'Logger']
--- meizen/transpile.py
+++ meizen/transpile.py
@@ -1,7 +1,7 @@
 from .dncl.lexer import lexical_analyse, parse
 from .util.run import run as util_run
 from .util.regEx import check_str, check_re
-from .util.log import logger
+from .util.log import logger, Logger
 from .util.io import load, write
 
 
@@ -16,28 +16,34 @@ def make(path: str, filename: str):
     build(path, filename)
 
 
-def build(filename: str, path: str = "./"):
+def build(filename: str, path: str = "./", log=None):
     """
     DNCLからPythonへの翻訳関数
     pathフォルダ内に存在するfilename.dnclを読み取り、同フォルダ内にfilename.pyを作成する
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス (default:"./")
     :param filename: 読み取るファイル名 (拡張子は除く)
     :return:
     """
-    logger(":----------翻訳を開始します----------:")
-    compile_code(path=path, filename=filename)
+    compile_code(path=path, filename=filename, log=log)
 
 
-def compile_code(path: str, filename: str):
+def compile_code(path: str, filename: str, log: Logger = None):
     """
     pathフォルダ内に存在するfilename.dnclを読み取り、同フォルダ内にfilename.pyを作成する
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename: 読み取るファイル名 (拡張子は除く)
     :return:
     """
+
     read_file_path = path + filename + ".dncl"
     write_file_path = path + filename + ".py"
     code_list: list = load(path=read_file_path)
+    if log is None:
+        logger(":----------翻訳を開始します----------:")
+    else:
+        log.dncl(msg=":----------翻訳を開始します----------:", code_list=code_list)
     # ここから 最後の１行を構文解析できないことのケアとして空行を追加する処理
     code_list[len(code_list) - 1] += "\n"
     code_list.append("")
@@ -47,19 +53,23 @@ def compile_code(path: str, filename: str):
     code_list = parse(code_word=code_word, code_symbol=code_symbol)
     logger("構文解析完了")
     write(path=write_file_path, code_list=code_list)
-    logger("書き込み完了")
+    if log is None:
+        logger("書き込み完了")
+    else:
+        log.dncl(msg="書き込み完了", code_list=code_list)
 
 
-def run(filename: str, path: str = "./") -> (str, str):
+def run(filename: str, path: str = "./", log=None) -> (str, str):
     """
     DNCLからPythonへの翻訳-実行関数
     pathフォルダ内に存在するfilename.dnclを読み取り、同フォルダ内にfilename.pyを作成し、filename.pyを実行する
     処理はdoに委譲
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename: 読み取るファイル名 (拡張子は除く)
     :return:
     """
-    do(path=path, filename=filename)
+    do(path=path, filename=filename, log=log)
 
 
 def make_run(path: str, filename: str):
@@ -72,30 +82,45 @@ def make_run(path: str, filename: str):
     run(path, filename)
 
 
-def do(path: str, filename: str) -> (str, str):
+def do(path: str, filename: str, log: Logger = None) -> (str, str):
     """
     DNCLからPythonへの翻訳-実行関数
     pathフォルダ内に存在するfilename.dnclを読み取り、同フォルダ内にfilename.pyを作成し、filename.pyを実行する
     作成部はbuildに委譲
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename: 読み取るファイル名 (拡張子は除く)
     :return: 実行結果の標準出力, エラー出力
     """
-    build(path=path, filename=filename)
-    result = util_run(path=path + filename + ".py")
-    return result
+    build(path=path, filename=filename, log=log)
+    std, err = util_run(path=path + filename + ".py")
+    logger(":----------処理を実行します----------:")
+    if log is None:
+        logger(":--------------標準出力-------------:")
+        logger(std)
+    else:
+        log.std_out(msg=":--------------標準出力-------------:", code=std)
+    if err is not "":
+        if log is None:
+            logger(":-------------エラー出力------------:")
+            logger(err)
+        else:
+            log.std_out(msg=":-------------エラー出力------------:", code=err)
+    logger(":------------実行しました-----------:")
+    return std, err
 
 
-def exam_re(answer: str, filename: str, path: str = "./"):
+def exam_re(answer: str, filename: str, path: str = "./", log=None):
     """
     変換と実行、実行結果の正規表現による確認まで行う関数
     python標準のreパッケージのfullmatchに基づく
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename:読み取るファイル名 (拡張子は除く)
     :param answer: 確認する正規表現
     :return:
     """
-    out, _ = do(path=path, filename=filename)
+    out, _ = do(path=path, filename=filename, log=log)
     result: bool = check_re(sentence=out, pattern=answer)
     logger(":----------出力を確認します----------:")
     if result:
@@ -106,16 +131,17 @@ def exam_re(answer: str, filename: str, path: str = "./"):
     logger(":-----------確認は以上です-----------:")
 
 
-def exam_str(answer: str, filename: str, path: str = "./"):
+def exam_str(answer: str, filename: str, path: str = "./", log=None):
     """
     変換と実行、実行結果の文字列による確認まで行う関数
     完全一致のみを正答とする
+    :param log: 記録用のクラス(meizen.Logger型)(default:None)
     :param path: 読み取りファイルと書き込みファイルの存在するディレクトリ(フォルダ)のパス
     :param filename:読み取るファイル名 (拡張子は除く)
     :param answer: 確認する文字列
     :return:
     """
-    out, _ = do(path=path, filename=filename)
+    out, _ = do(path=path, filename=filename, log=log)
     result: bool = check_str(sentence=out, string=answer)
     logger(":----------出力を確認します----------:")
     if result:
