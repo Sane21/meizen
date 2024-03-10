7271f2289c08a17c92faefdcba5ba65f8785d5da
Sane21 <72343236+Sane21@users.noreply.github.com>
2024-03-11 01:07:27 +0900

    update description

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='meizen',
     version='0.3.2',
-    description="This package is designed to transcompile the Common Test Procedure Description Standard Language ("
-                "DNCL) established by the University Entrance Examination Center for the \"Information\" section of "
-                "the university entrance common test in Python. It aims to support the learning of high school "
+    description="This package is designed to transcompile the Programming Expressions for the Common Test(call:"
+                "DNCL2) established by the University Entrance Examination Center for the \"Information\" section of "
+                "the university entrance common test to Python. It aims to support the learning of high school "
                 "students in Japan.",
     author="Keita Sanefuji",
     author_email="sane21.edu@gmail.com",
